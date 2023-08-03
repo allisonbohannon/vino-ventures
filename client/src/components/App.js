d3ee10ea8bd5fc77997f2e4411e2396801e2affc
Allison Bohannon <akboh.github@gmail.com>
2023-08-03 09:32:12 -0700

    Update folder structure

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+
+import { useState, useEffect, useContext } from "react";
+import { Route, Routes } from "react-router-dom";
+
+
+function App() {
+
+    return (
+        <div>Hello, world</div>
+    )
+
+}
+
+export default App; 
