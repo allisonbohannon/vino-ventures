90e5f51f5a2d93449d6eca1fe5f48b90ca8ee0a8
Allison Bohannon <akboh.github@gmail.com>
2023-08-18 15:03:25 -0400

    Add new columns

--- app/serializers/winery_serializer.rb
+++ app/serializers/winery_serializer.rb
@@ -1,3 +1,3 @@
 class WinerySerializer < ActiveModel::Serializer
-  attributes :id, :name, :about, :tastingcost, :rezrequired, :imagesrc, :address1, :address2, :city
+  attributes :id, :name, :about, :tastingcost, :rezrequired, :imagesrc, :address1, :address2, :city, :region, :website, :avg_ratings, :total_ratings
 end
